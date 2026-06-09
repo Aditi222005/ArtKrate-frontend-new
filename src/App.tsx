bb8a9d70d895341bcd45d957e12fbd3a3e097f4f
joshiAditi2005 <aditisjoshi2005@gmail.com>
2026-06-10 02:26:02 +0530

    deployment environement created

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ import PageTransition from "./components/PageTransition";
 
 // Global axios config
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://vercel.com/aditisjoshi2005-gmailcoms-projects/artkrate-backend-new/HPWRxYfCzgLgbakweRAHEz2tAKi1";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "https://artkrate-backend-new.vercel.app";
 
 const queryClient = new QueryClient({
   defaultOptions: {
